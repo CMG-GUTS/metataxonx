3547d4863cb01ad29619205c41ce8757602e858f
alemg <alem.gusinac@radboudumc.nl>
2024-05-02 16:18:51 +0200

    Generate a qiime mapping format, according to metadata

--- bin/python/run_cutadapt.py
+++ bin/python/run_cutadapt.py
@@ -7,34 +7,55 @@ run_cutadapt
 .. module:: run_cutadapt
   :synopsis: run cutadapt on paired-end illumina-style data
 .. moduleauthor:: Jos Boekhorst
+.. Adapted by Alem Gusinac, 02-05-2024
 
 run_cutadapt.py
 
 Typical run::
 
-    run_cutadapt.py -f FWD_PRIMER -r REV_PRIMER -c 64 *.fastq.gz
+    run_cutadapt.py -f FWD_PRIMER -r REV_PRIMER -c 64 -m metadata.tsv -s 'paired' --pear 'yes'
 
 Run the script with '-h' for a list of options.
 
 """
 
 import argparse
-import sys
-import os
-import subprocess
+import sys, os, glob
+from utils.tools import get_metadata, generate_qiime_mapping, get_file_counts
 
+def write_counts(metadata, outfolder, outname, reverse=None):
+    # Fetch raw and trimmed files
+    untrimmed = glob.glob("*R1*")
+    trimmed = [os.path.basename(file) for file in glob.glob(f"{outfolder}/*R1*")]
+    samples = metadata['SAMPLE-ID'].tolist()
+    if reverse:
+        untrimmed += glob.glob("*R2*")
+        trimmed += [os.path.basename(file) for file in glob.glob(f"{outfolder}/*R2*")]
+        samples += metadata['SAMPLE-ID'].tolist()
 
-def write_counts(infiles, outfolder, outname):
-    infiles = [element for element in infiles if "_R2" not in element]
+    # sort files
+    untrimmed.sort()
+    trimmed.sort()
+    samples.sort()
+
+    # Writes output
     with open(outname, 'w') as f:
         f.write("\t".join(['sample', 'input', 'trimmed']) + '\n')
-        for infile in infiles:
-            sample = infile.split("_")[0]
-            result = subprocess.run([f'zcat {infile}|wc'], shell=True, capture_output=True, text=True)
-            precount = "%i" % (int(result.stdout.split()[0]) / 4)
-            result = subprocess.run([f'zcat {outfolder}/{infile}|wc'], shell=True, capture_output=True, text=True)
-            postcount = "%i" % (int(result.stdout.split()[0]) / 4)
-            f.write("\t".join([sample, str(precount), str(postcount)]) + '\n')
+        for (sample_name, untrim_file, trim_file) in zip(samples, untrimmed, trimmed):
+            if untrim_file.startswith(sample_name) and trim_file.startswith(sample_name):
+                precount = get_file_counts(untrim_file)
+                postcount = get_file_counts(f'{outfolder}/{trim_file}')
+                f.write("\t".join([sample_name, str(precount), str(postcount)]) + '\n')
+
+def run_cutadapt(sample, forward, options, reverse=None):
+    if reverse:
+        command = f"cutadapt -j {options['cpus']} --discard-untrimmed -g {options['fw']} -G {options['rev']} -o trimmed/{sample}_R1.fastq.gz -p trimmed/{sample}_R2.fastq.gz {forward} {reverse}"
+    else:
+        command = f"cutadapt -j {options['cpus']} --discard-untrimmed -g {options['fw']} -o trimmed/{sample}_R1.fastq.gz {forward}"
+    result = os.system(command)
+    if result != 0:
+        sys.stderr.write("Error running cutadapt, aborting\n")
+        sys.exit(1)
 
 
 ############
@@ -47,19 +68,26 @@ description = "run cutadapt on paired-end illumina-style data"
 # main program
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=description, add_help=True, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-f', dest='fwd', help='fwd primer sequence', required=True)
+    parser.add_argument('-f', dest='fw', help='fwd primer sequence', required=True)
     parser.add_argument('-r', dest='rev', help='rev primer sequence', required=True)
+    parser.add_argument('-m', dest="metadata", help='metadata file', required=True)
+    parser.add_argument('-s', dest="seq_read", help='single or paired end sequence reading', required=True)
+    parser.add_argument('--pear', dest="pear", help="PEAR 'yes' or 'no'", required=True)
     parser.add_argument('-c', dest="cpus", help='number of cpus to us', default=1)
-    parser.add_argument('input_files', help="input files", nargs='+')
     options = vars(parser.parse_args())
 
     os.mkdir("trimmed")
-    names = [element.split("_")[0] for element in options['input_files'] if "R1" in element]
-    for name in names:
-        command = f"cutadapt -j {options['cpus']} --discard-untrimmed -g {options['fwd']} -G {options['rev']} -o trimmed/{name}_R1.fastq.gz -p trimmed/{name}_R2.fastq.gz {name}_R1.fastq.gz {name}_R2.fastq.gz"
-        result = os.system(command)
-        if result != 0:
-            sys.stderr.write("Error running cutadapt, aborting\n")
-            sys.exit(1)
-    write_counts(options['input_files'], "trimmed", "cutadapt_counts.txt")
-    sys.exit()
+    metadata = get_metadata(options['metadata'])
+    # Run cutadapt, create qiime mapping file and summarize counts for either paired or single end sequences
+    if options['seq_read'] == "paired" or options['pear'] == "yes":
+        for (sample, fw, rev) in zip(metadata['SAMPLE-ID'], metadata['FILENAME_fw'], metadata['FILENAME_rev']):
+            run_cutadapt(sample=sample, forward=fw, reverse=rev, options=options)
+            generate_qiime_mapping(metadata=metadata, file_string1="R1", file_string2="R2", outdir="trimmed", reverse=True)
+            write_counts(metadata, "trimmed", "cutadapt_counts.txt", reverse=True)
+
+    elif options['seq_read'] == 'single' and options['pear'] == "no":
+        for (sample, fw) in zip(metadata['SAMPLE-ID'], metadata['FILENAME_fw']):
+            run_cutadapt(sample=sample, forward=fw, options=options)
+            generate_qiime_mapping(metadata=metadata, file_string1="R1", file_string2="R2", outdir="trimmed", reverse=False)
+            write_counts(metadata, "trimmed", "cutadapt_counts.txt", reverse=False)
+    sys.exit(0)
--- bin/python/run_pear.py
+++ bin/python/run_pear.py
@@ -1,52 +1,81 @@
 #!/usr/bin/env python3
-"""minimal Python wrapper for Pear to prevent doing stuff in nextflow Java"""
+
+"""
+run_pear
+---------------
+.. module:: run_pear
+  :synopsis: Runs pear from command line and generates qiime format mapping file
+.. moduleauthor:: Jos Boekhorst 
+.. adapted by Alem Gusinac at 02-05-2024
+
+Generate mapping.txt and pear_counts.txt
+
+Typical run::
+
+    run_pear.py -m metadata.tsv
+
+Run the script with '-h' for a list of options.
+
+"""
+
 import glob
 import os
 import sys
-import subprocess
+import argparse
+from utils.tools import get_file_counts, generate_qiime_mapping, get_metadata
 
+def write_counts(metadata, outfolder, outname):
+    # Fetch raw and trimmed files
+    input_R1 = glob.glob("*R1*")
+    assembled = [os.path.basename(file) for file in glob.glob(f"{outfolder}/*fastq.gz")]
+    samples = metadata['SAMPLE-ID'].tolist()
 
-def write_counts(infiles, outfolder, outname):
+    # sort files
+    input_R1.sort()
+    assembled.sort()
+    samples.sort()
+
+    # Writes output
     with open(outname, 'w') as f:
         f.write("\t".join(['sample', 'input', 'assembled']) + '\n')
-        for infile in infiles:
-            sample = infile.split("_")[0]
-            result = subprocess.run([f'zcat {infile}|wc'], shell=True, capture_output=True, text=True)
-            precount = "%i" % (int(result.stdout.split()[0]) / 4)
-            result = subprocess.run([f'zcat {outfolder}/{infile.replace("_R1", "")}|wc'], shell=True, capture_output=True, text=True)
-            postcount = "%i" % (int(result.stdout.split()[0]) / 4)
-            f.write("\t".join([sample, str(precount), str(postcount)]) + '\n')
+        for (sample_name, input_file, assembled_file) in zip(samples, input_R1, assembled):
+            if input_file.startswith(sample_name) and assembled_file.startswith(sample_name):
+                precount = get_file_counts(input_file)
+                postcount = get_file_counts(f"{outfolder}/{assembled_file}")
+                f.write("\t".join([sample_name, str(precount), str(postcount)]) + '\n')
 
+def run_pear(sample, forward, reverse):
+    command = f"pear -f {forward} -r {reverse} -o {sample} -y 25G -j 32 -q 30 -v 35 -p 0.0001 > {sample}_pear_stats.txt"""
+    result = os.system(command)
+    if result != 0:
+        sys.stderr.write("Error running pear, aborting\n")
+        sys.exit(2)
 
 if __name__ == "__main__":
-    all_files = glob.glob("*fastq*")
+    parser = argparse.ArgumentParser(description="PEAR argument parser", add_help=True, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-m', dest="metadata", help='metadata file', required=True)
+    options = vars(parser.parse_args())
+
+    # Creates folder
     os.mkdir("assembled")
-    for name in all_files:
-        fwd = ""
-        rev = ""
-        if name.find("_R1.") != -1:
-            fwd = name
-            rev = name.replace("_R1.", "_R2.")
-        elif name.find("_1.") != -1:
-            fwd = name
-            rev = name.replace("_1.", "_2.")
-        nicename = '_'.join(name.split('.')[0].split('_')[:-1])
-        if fwd != "":
-            command = f"""pear \
-            -f {fwd} \
-            -r {rev} \
-            -o {nicename} \
-            -y 25G \
-            -j 32 \
-            -q 30 \
-            -v 35 \
-            -p 0.0001 \
-            > {nicename}_pear_stats.txt"""
-            print(command)
-            result = os.system(command)
-            if result != 0:
-                sys.stderr.write("Error running pear, aborting\n")
-                sys.exit(2)
-            os.system(f'mv {nicename}.assembled.fastq assembled/{nicename}.fastq')
-            os.system(f'gzip assembled/{nicename}.fastq')
-    write_counts(glob.glob("*R1.fastq.gz"), 'assembled', 'pear_counts.txt')
+
+    # Fetch metadata, forward and reverse files
+    fw_files = glob.glob("*R1*")
+    rev_files = glob.glob("*R2*")
+    metadata = get_metadata(options['metadata'])
+    samples = metadata['SAMPLE-ID'].tolist()
+
+    # sort files accordingly
+    fw_files.sort()
+    rev_files.sort()
+    samples.sort()
+
+    # Run PEAR on sorted samples & files (using sample-id as naming)
+    for (sample, fw, rev) in zip(samples, fw_files, rev_files):
+        run_pear(sample=sample, forward=fw, reverse=rev)
+        os.system(f'mv {sample}.assembled.fastq assembled/{sample}.fastq')
+        os.system(f'gzip assembled/{sample}.fastq')
+
+    # Generate stats and mapping files
+    generate_qiime_mapping(metadata=get_metadata(options['metadata']), file_string1="fastq", file_string2="", outdir="assembled", reverse=False)
+    write_counts(metadata, 'assembled', 'pear_counts.txt')
