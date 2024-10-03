99ef866dccfa6ae0fc7e9c6133df08185e584d79
alemg <alem.gusinac@radboudumc.nl>
2024-10-03 13:31:43 +0200

    Improved code creates for either single or paired end

--- bin/python/merge_readstats.py
+++ bin/python/merge_readstats.py
@@ -5,109 +5,85 @@
 merge_readstats
 ---------------
 .. module:: merge_readstats
-  :synopsis: merge Qiime2 nextflow read accounting data
+  :synopsis: merge parallel_dada2, pear, cutadapt read stats
 .. moduleauthor:: Jos Boekhorst
+.. adapted by Alem Gusinac at 03-10-2024
 
-Add Pear-assembler and cutadapt statistics to the dada2_denoise stats file of qiime2
+Add Pear-assembler and cutadapt statistics to the dada2_denoise stats file of parallel_dada2
 
 Typical run::
 
-    merge_readstats -d denoising_stats.txt -i fastq/
+    merge_readstats -d denoising_stats.txt -s paired
 
 Run the script with '-h' for a list of options.
 """
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-
-
-def read_stats(filename, relevants):
-    """read counts from txt file"""
-    with open(filename, 'r') as f:
-        lines = f.read().rstrip().split('\n')
-    lines = [element for element in lines if element[0] != "#"]  # skip comments
-    header = lines[0].split('\t')
-    data = {}
-    for line in lines[1:]:  # header skipped
-        lineg = line.split('\t')
-        sample = lineg[0]
-        data[sample] = {}
-        for element in relevants:
-            try:
-                i = header.index(element)
-                data[sample][element] = lineg[i]
-            except ValueError:  # header not in this file
-                pass
-    return data
-
-
-def add_fractions(data, max_header):
-    """after each header add percentage of input"""
-    new_data = {}
-    final_header = []
-    for sample in data:
-        final_header = []
-        new_data[sample] = {}
-        new_data[sample]["input"] = data[sample]['input']
-        for column in max_header[1:]:  # skip the "input"
-            if column in data[sample]:
-                final_header.append(column)
-                new_data[sample][column] = data[sample][column]
-                final_header.append(f"{column}%")
-                new_data[sample][f"{column}%"] = "%.2f" % (
-                            float(data[sample][column]) / float(data[sample]['input']) * 100)
-    return new_data, final_header
-
-
-def write_stats(filename, new_data, final_header):
-    with open(filename, 'w') as f:
-        f.write("\t".join(["sample"] + final_header) + '\n')
-        for sample in new_data:
-            f.write("\t".join([sample] + [new_data[sample][element] for element in final_header]) + '\n')
-
-
-def combine(denoise_data, cutadapt_data=None, pear_data=None):
-    combined_data = denoise_data
-    if cutadapt_data is not None:
-        for sample in cutadapt_data:
-            combined_data[sample]['input'] = cutadapt_data[sample]['input']
-            combined_data[sample]['trimmed'] = cutadapt_data[sample]['trimmed']
-    if pear_data is not None:
-        if cutadapt_data is None:
-            for sample in pear_data:
-                combined_data[sample]['input'] = pear_data[sample]['input']
-        for sample in pear_data:
-            combined_data[sample]['assembled'] = pear_data[sample]['assembled']
-    return combined_data
-
+import pandas as pd
 
 ############
 # SETTINGS #
 ############
-# note: default parameters are set in argparse object (top of __main__)
 description = "Add Pear-assembler and cutadapt statistics to the dada2_denoise stats file of qiime2"
-starting_header_relevants = ["sample", "input", "denoised", "merged", "non-chimeric", "trimmed", "assembled"]
-max_header = ["sample", "input", "trimmed", "assembled", "denoised", "merged", "non-chimeric"]
+single_headers = ["sample", "input", "trimmed", "trimmed%", "filtered", "filtered%", "denoised", "denoised%", "nonchim", "nonchim%"]
+paired_headers = ["sample", "input", "trimmed", "trimmed%", "assembled", "assembled%", "filtered", "filtered%", "denoised", "denoised%", "nonchim", "nonchim%"]
 
 # main program
 if __name__ == '__main__':
     parser = ArgumentParser(description=description, add_help=True,
                                      formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-d', dest='denoisefile', help='denoise_stats.txt file', default="denoising_stats.txt")
-    parser.add_argument('-c', dest='cutadaptfile', help='cutadapt counts file', default="NA")
-    parser.add_argument('-p', dest='pearfile', help='pear counts file', default="NA")
-    parser.add_argument('-o', dest='outfile', help='output filename', default="counts.txt")
+    parser.add_argument('-d', dest='denoisefile', help='denoise_stats.txt file', default=False)
+    parser.add_argument('-c', dest='cutadaptfile', help='cutadapt counts file', default=False)
+    parser.add_argument('-p', dest='pearfile', help='pear counts file', default=False)
+    parser.add_argument('-o', dest='outfile', help='output filename', default=False)
+    parser.add_argument('-s', dest="seq_read", help="paired or single -end reads", required=True)
 
     options = vars(parser.parse_args())
 
-    denoise_data = read_stats(options['denoisefile'], starting_header_relevants)
-    if options['cutadaptfile'] != "NA":
-        cutadapt_data = read_stats(options['cutadaptfile'], starting_header_relevants)
-    else:
-        cutadapt_data = None
-    if options['pearfile'] != "NA":
-        pear_data = read_stats(options['pearfile'], starting_header_relevants)
-    else:
-        pear_data = None
-    combined_data = combine(denoise_data, cutadapt_data=cutadapt_data, pear_data=pear_data)
-    final_data, final_header = add_fractions(combined_data, max_header)
-    write_stats(options['outfile'], final_data, final_header)
+    # Fetch files if present
+    if options["cutadaptfile"]:
+        cutadapt_stats = pd.read_csv(options["cutadaptfile"], sep = "\t")
+
+    if options["denoisefile"]:
+        denoise_stats = pd.read_csv(options["denoisefile"], sep = "\t")
+        denoise_stats["sample"] = denoise_stats.index
+        denoise_stats = denoise_stats.drop(columns = ["input"])
+
+    if options["pearfile"]:
+        pear_stats = pd.read_csv(options["pearfile"], sep = "\t")
+        pear_stats = pear_stats.drop(columns = ["input"])
+
+    # Combine stats for single-end seq
+    if options["seq_read"] == "single":
+        # merge single stats
+        single_stats = cutadapt_stats.merge(denoise_stats, on = "sample")
+        baseline = single_stats["input"]
+
+        # Compute percentage differences
+        single_stats["trimmed%"] = single_stats["trimmed"] / baseline * 100
+        single_stats["filtered%"] = single_stats["filtered"] / baseline * 100
+        single_stats["denoised%"] = single_stats["denoised"] / baseline * 100
+        single_stats["nonchim%"] = single_stats["nonchim"] / baseline * 100
+
+        # rearranges column order
+        single_stats_final = single_stats[single_headers]
+
+        # outputs tsv file
+        single_stats_final.to_csv("read_stats.tsv", sep = "\t", index = False)
+
+    if options["seq_read"] == "paired":
+        paired_stats = cutadapt_stats.merge(pear_stats, on = "sample").merge(denoise_stats, on = "sample")
+        baseline = paired_stats["input"]
+
+        # Compute percentage differences
+        paired_stats["trimmed%"] = paired_stats["trimmed"] / baseline * 100
+        paired_stats["assembled%"] = paired_stats["assembled"] / baseline * 100
+        paired_stats["filtered%"] = paired_stats["filtered"] / baseline * 100
+        paired_stats["denoised%"] = paired_stats["denoised"] / baseline * 100
+        paired_stats["nonchim%"] = paired_stats["nonchim"] / baseline * 100
+
+        # rearranges column order
+        paired_stats_final = paired_stats[paired_headers]
+
+        # outputs tsv file
+        paired_stats_final.to_csv("read_stats.tsv", sep = "\t", index = False)
